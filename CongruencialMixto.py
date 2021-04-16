b10b916c856dd0c21c1074626427ef23f61cd466
Fabricio <fabricio.fuentes12@gmail.com>
2021-04-16 10:52:41 -0500

    Congruencial mixto

--- CongruencialMixto.py
+++ CongruencialMixto.py
@@ -9,54 +9,60 @@ class CongruencialMixto:
         self.semilla = semilla
 
     def generador(self, randomNum):
-        nextRandom = ((randomNum * self.multiplicativo) +
-                      self.incremento) % self.modulo
+        nextRandom = ((randomNum * self.multiplicativo) + self.incremento) % self.modulo
 
         return nextRandom
 
-    def cicloDeGeneradores(self, noRandomNum, q):
+    def cicloDeGeneradores(self, noRandomNum):
         nextRandom = self.semilla
         randomNum = []
 
-        if self.hullDobell(q):
+        if self.hullDobell():
             for i in range(0, noRandomNum):
                 newRandom = self.generador(nextRandom)
-                randomNum.append(float(self.generador(nextRandom))/self.modulo)
+                randomNum.append(float(self.generador(nextRandom)) / self.modulo)
                 nextRandom = newRandom
 
         print(randomNum)
 
     def checarPrimo(self, num):
-        if num > 1:
-            for i in range(2, num):
-                if (num % i) == 0:
-                    return False
-            else:
-                return True
-        else:
-            return False
+        primos = []
+        while num % 2 == 0:
+            primos.append(2)
+            num /= 2
 
-    def hullDobell(self, q):
+        for i in range(3, int(math.sqrt(num)) + 1, 2):
+            while num % i == 0:
+                primos.append(i)
+                num /= i
+
+        if num > 2:
+            primos.append(num)
+
+        return primos
+
+    def hullDobell(self):
         # i) Sea c y m primos relativo (el máximo común divisor entero c y m es 1)
         if math.gcd(self.incremento, self.modulo) == 1:
             print("Primer chequeo de Hull-Dobell pasado")
 
             # ii) Si q es un número primo que divide a m ; entonces, q divide a (a-1) 𝑎≡1𝑚𝑜𝑑𝑞
-            if self.checarPrimo(q):
-                print((self.multiplicativo-1)/q)
-                print("Segundo chequeo de Hull-Dobell pasado")
+            for q in self.checarPrimo(self.modulo):
+                if (self.modulo % q == 0) and ((self.multiplicativo - 1) % q == 0):
+                    print(q)
+                    print("Segundo chequeo de Hull-Dobell pasado")
+                    break
 
-                # iii) Si 4 divide a m ; entonces, 4 divide a (a-1). Es decir, 𝑎≡1𝑚𝑜𝑑4
-                if self.modulo % 4 == 0:
-                    print((self.multiplicativo-1)/4)
-                    print("Hull-Dobell pasado!")
-                    return True
+            # iii) Si 4 divide a m ; entonces, 4 divide a (a-1). Es decir, 𝑎≡1𝑚𝑜𝑑4
+            if self.modulo % 4 == 0 and ((self.multiplicativo - 1) % 4 == 0):
+                print("Hull-Dobell pasado!")
+                return True
         else:
             print("No paso el chequeo Hull-Dobell")
             return False
 
 
-'''
+"""
 cmg = CongruencialMixto(5, 7, 8, 4)
-cmg.cicloDeGeneradores(20, 2)
-'''
+cmg.cicloDeGeneradores(20)
+"""
